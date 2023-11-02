92d85d2dc449406c7e7e6089d478bb5c9b0088a0
xandro2021 <xandrocastro2021@gmail.com>
2023-11-01 20:34:16 -0600

    Crear el context requerido para este proyecto

---
+++ src/context/ClimaProvider.jsx
@@ -0,0 +1,38 @@
+import { useState, createContext } from 'react';
+
+const ClimaContext = createContext();
+
+const ClimaProvider = ({ children }) => {
+
+  const [busqueda, setBusqueda] = useState({
+    ciudad: '',
+    pais: '',
+  });
+
+  const datosBusqueda = e => {
+    setBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const consultarClima = datos => {
+    console.log(datos);
+  };
+
+  return (
+    <ClimaContext.Provider
+      value={{
+        busqueda,
+        datosBusqueda,
+        consultarClima,
+      }}
+    >
+      {children}
+    </ClimaContext.Provider>
+  );
+};
+
+export { ClimaProvider };
+
+export default ClimaContext;
